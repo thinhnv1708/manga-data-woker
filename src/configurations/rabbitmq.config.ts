720a443cfaab6d76d6453f920c91aaa00a92ae99
thinhnv <thinhnv@gviet.vn>
2024-01-12 14:31:18 +0700

    UPDATE: env

--- src/configurations/rabbitmq.config.ts
+++ src/configurations/rabbitmq.config.ts
@@ -6,7 +6,7 @@ export default (): {
   RABBITMQ_CONFIG: {
     PROTOCOL: process.env.RABBITMQ_PROTOCOL || 'amqp',
     BROKERS: process.env.RABBITMQ_BROKERS?.split(' ') || ['localhost:5672'],
-    USERNAME: process.env.RABBITMQ_USERNAME || 'admin',
+    USERNAME: process.env.RABBITMQ_USER || 'admin',
     PASSWORD: process.env.RABBITMQ_PASSWORD || 'admin',
     VHOST: process.env.RABBITMQ_VHOST || 'manga',
     QUEUE_NAME: process.env.RABBITMQ_QUEUE_NAME || 'manga',
