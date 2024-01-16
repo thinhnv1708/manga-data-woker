c8a8df500671a62bb649b1a6632962425d4259c8
thinhnv <thinhnv@gviet.vn>
2024-01-16 16:09:29 +0700

    UPDATE: completedCrawler

--- src/core/useCases/chapter/chapterManager.useCase.ts
+++ src/core/useCases/chapter/chapterManager.useCase.ts
@@ -58,7 +58,8 @@ export class ChapterManagerUseCase {
     }
 
     const chapterId = chapter.getId();
-    const completedCrawler = pages?.length > 0 ? true : false;
+    const completedCrawler = pages?.length > 0;
+
     await this.chapterRepository.updatePagesInChapter(
       chapterId,
       pages,
