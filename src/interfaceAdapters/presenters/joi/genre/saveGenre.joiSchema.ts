f13b43fcf720f61aaa4bdaa50aab0c38bd9358ce
ThinhNV <thinhnv1708@gmail.com>
2024-01-16 22:48:36 +0700

    UPDATE

--- src/interfaceAdapters/presenters/joi/genre/saveGenre.joiSchema.ts
+++ src/interfaceAdapters/presenters/joi/genre/saveGenre.joiSchema.ts
@@ -2,6 +2,6 @@ import { ISaveGenreInput } from '@core/dtos/abstracts/genre';
 import * as Joi from 'joi';
 
 export const saveGenreJoiSchema = Joi.object<ISaveGenreInput>({
-  title: Joi.string().allow(''),
+  title: Joi.string().allow(null),
   path: Joi.string().required(),
 });
